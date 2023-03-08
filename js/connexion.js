83864971c3026b0e27104c758d95350a8903d755
amelie.vergnaud <hasphodelle49@outlook.fr>
2023-03-08 23:34:51 +0100

    Je te rick roll Lucas (never gonna give you upppp)

--- js/connexion.js
+++ js/connexion.js
@@ -12,7 +12,7 @@ function ins(){
     document.getElementById("patate").innerHTML =`<div id="SideInscription">
     <form action="#" method="post">
         <input type="text" name="username" id="username" placeholder="Nom d'utilisateur">
-        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="text" name="nickname" id="nickname" placeholder="Pseudo">
         <input type="password" name="password" id="password" placeholder="Mot de Passe">
         <button type="submit">S'inscrire</button>
     </form>
