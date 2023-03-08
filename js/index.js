b88f9b1596b6e616d9c82fa8dfccd83be7c96089
Nonokaaa <noestm77@gmail.com>
2023-03-08 17:30:43 +0100

    ajout du popup de suppression de compte

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//onglets du menu
 function ongletsMenu(menu){
     menuElements = document.getElementsByClassName('onglet');
     for (const tab of menuElements){
@@ -12,4 +13,20 @@ function ongletsMenu(menu){
         }
     }
     document.getElementById(`${menu}`).classList.remove('none');
+}
+
+//popup suppression de compte
+popup = document.getElementById('popup-fond');
+html = document.getElementsByTagName('html');
+
+//permet d'afficher le popup
+function affiche(){
+    html[0].style.overflowY='hidden';
+    popup.classList.toggle('none');
+}
+
+//permet de fermer le popup
+function quit(){
+    html[0].style.overflowY='visible';
+    popup.classList.toggle('none');
 }
