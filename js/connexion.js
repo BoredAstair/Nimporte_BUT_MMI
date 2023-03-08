c2925d5c5ad11441fcc8178b88e2474f6aaddbc4
amelie.vergnaud <hasphodelle49@outlook.fr>
2023-03-08 23:54:07 +0100

    J'ai mis une erreur pour toi Tessa <3

--- js/connexion.js
+++ js/connexion.js
@@ -1,6 +1,7 @@
 function con(){
     document.getElementById("patate").innerHTML =`<div id="SideConnexion">
     <form action="#" method="post">
+        <p>Tu peux mettre l'erreur ici Tessa</p>
         <input type="text" name="username" id="username" placeholder="Nom d'utilisateur">
         <input type="password" name="password" id="password" placeholder="Mot de Passe">
         <button type="submit">Se Connecter</button>
