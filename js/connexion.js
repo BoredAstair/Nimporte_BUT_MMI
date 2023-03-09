dc2a53b2214b0da9930bceb36b1cfd9a12d3b3d4
Tessa Germain <tessa.germain2003@gmail.com>
2023-03-09 09:01:48 +0100

    scss connexion + lien api/inscription

--- js/connexion.js
+++ js/connexion.js
@@ -1,21 +1,45 @@
 function con(){
     document.getElementById("patate").innerHTML =`<div id="SideConnexion">
-    <form action="api/api.php/post/login" method="post">
+    <div id="form">
+        <p>Tu peux mettre l'erreur ici Tessa</p>
         <input type="text" name="username" id="username" placeholder="Nom d'utilisateur">
         <input type="password" name="password" id="password" placeholder="Mot de Passe">
         <button type="submit">Se Connecter</button>
-    </form>
+    </div>
 </div>`;
 }
 
 function ins(){
     document.getElementById("patate").innerHTML =`<div id="SideInscription">
-    <form action="api/api.php/post/inscription" method="post">
+    <div id="form">
         <input type="text" name="username" id="username" placeholder="Nom d'utilisateur">
-        <input type="pseudo" name="pseudo" id="pseudo" placeholder="Pseudo">
+        <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
         <input type="password" name="password" id="password" placeholder="Mot de Passe">
-        <button type="submit">S'inscrire</button>
-    </form>
+        <button onclick="requeteInscription()" type="submit">S'inscrire</button>
+    </div>
 </div>`;
 }
-con();
+
+//REQUETE
+function requeteInscription(){
+    console.log("lancement requête");
+    let username = document.getElementById("username").value;
+    let pseudo = document.getElementById("pseudo").value;
+    let password = document.getElementById("password").value;
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = traitementInscription;
+    httpRequest.open('POST', 'http://localhost/owlTree/Nimporte_BUT_MMI/api/api.php/post/inscription', true);
+    httpRequest.setRequestHeader("Content-Type", "application/json");
+    var data = JSON.stringify({"username": username, "pseudo": pseudo, "password": password});
+    httpRequest.send(data);
+}
+function traitementInscription(){
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            console.log("requête réussie");
+        } 
+        else {
+            alert('Il y a eu un problème avec la requête.');
+        }
+    }       
+}
