ba3de1111c6a0b83ac09f57d6b48c701ef50870a
BoredAstair <103356497+BoredAstair@users.noreply.github.com>
2023-03-14 12:10:17 +0100

    reworked verifToken

--- api/function.php
+++ api/function.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 //TOUTES LES FONCTIONS UTILITAIRES TRIE PAR ORDRE ALPHABETIQUE
 
 function debug($tableau){ // affichage des tableaux pour débuger
@@ -12,33 +13,25 @@ function encodeJson($tableau){ // encode un tableau en format JSON
     echo $json_data;                 
 }
 function generateToken($longueur){ //génère un token
-/*$caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $longueurMax = strlen($caracteres);
-    $random ='';
-    for($i=0; $i<$longueur; $i++){
-        $random .= $caracteres[rand(0, $longueurMax - 1)];
-    }*/
     $random = random_bytes($longueur);
     $json_data = json_encode($random);
     echo $json_data;
 }
 function verifToken(){ //Vérifie si un token existe chez un utilisateur et si ce dernier est encore valable
     include("request.php");
-    if (isset($_COOKIE['tokenCookie'])){ //Verif presence token COOKIES
-        $response = selectCondition("token,token_date", "user", "user.token = '{$_COOKIE['tokenCookie']}'"); 
-        if ($_COOKIE['tokenCookie'] == $response[0]["token"]){ //Verif correspondance token BDD et token COOKIES
-            if (strtotime($response[0]['token_date']) < strtotime("-2 day")){
-            header("Location: ../index.html");
-            die();
+    $userID = null;
+    $isAuth = false;
+    foreach(getallheaders() as $name => $value){
+        if($name == "autorization"){
+            $res = selectCondition("*", "session", "token = {$value}");
+            if(sizeof($res) == 0){
+                http_response_code(401);
+                echo "the token is not valid";
+            } else {
+                $isAuth = true;
+                $userID = $res[0]['userID'];
             }
-        } else {
-            
-            header("Location: ../index.html");
-            die();
         }
-    } else {
-        header("Location: ../index.html");
-        die(); 
     }
 }
 ?>
